7acc1ad46b11e745a78bd5dfe8275bae1ff1766c
Peter Bjorklund <piot@hotmail.com>
2024-09-22 21:09:30 +0200

    fix: minor

--- crates/client-connecting/src/lib.rs
+++ crates/client-connecting/src/lib.rs
@@ -2,6 +2,7 @@
  * Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/nimble-rust/nimble
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  */
+use log::info;
 use nimble_protocol::prelude::*;
 use nimble_protocol::ClientRequestId;
 use std::fmt;
@@ -76,7 +77,10 @@ impl ConnectingClient {
         self.connected_info = Some(ConnectedInfo {
             session_connection_secret: cmd.host_assigned_connection_secret,
         });
-        //info!("connected: session_secret: {:?}", self.connected_info.unwrap());
+        info!(
+            "connected: session_secret: {:?}",
+            self.connected_info.unwrap()
+        );
         Ok(())
     }
 
