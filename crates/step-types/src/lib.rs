7b49ceb26fe40ed8af50d8a24f27f217640bc80e
Peter Bjorklund <piot@hotmail.com>
2024-10-15 22:30:57 +0200

    fix: rename to nimble-step-map

--- crates/step-types/src/lib.rs
+++ crates/step-types/src/lib.rs
@@ -2,30 +2,33 @@
  * Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/nimble-rust/nimble
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  */
-use nimble_participant::ParticipantId;
+use nimble_step_map::ParticipantId;
 use seq_map::SeqMap;
 use std::fmt::{Display, Formatter};
 
 #[derive(Debug, Default, Eq, PartialEq, Clone)]
-pub struct StepForParticipants<StepT: Display> {
-    pub combined_step: SeqMap<ParticipantId, StepT>,
+pub struct StepMap<StepT: Display> {
+    seq_map: SeqMap<ParticipantId, StepT>,
 }
-impl<StepT: Display> StepForParticipants<StepT> {
-    pub fn new() -> Self {
-        Self {
-            combined_step: SeqMap::new(),
+
+impl<StepT: Display> StepMap<StepT> {
+    /// Creates a new `StepMap`, returning `None` if the given [`SeqMap`] is empty.
+    pub fn new(seq_map: SeqMap<ParticipantId, StepT>) -> Option<Self> {
+        if seq_map.is_empty() {
+            None
+        } else {
+            Some(Self { seq_map })
         }
     }
 
-    pub fn is_empty(&self) -> bool {
-        self.combined_step.is_empty()
+    /// Provides a reference to the inner [`SeqMap`] holding the steps.
+    pub fn inner(&self) -> &SeqMap<ParticipantId, StepT> {
+        &self.seq_map
     }
 }
 
-impl<StepT: Display> Display for StepForParticipants<StepT> {
+impl<StepT: Display> Display for StepMap<StepT> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.combined_step)
+        write!(f, "{}", self.seq_map)
     }
 }
-
-pub type LocalIndex = u8;
