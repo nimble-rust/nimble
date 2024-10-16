02000f1f6c9bb4b3b51109f58d28cc328b5fde23
Peter Bjorklund <piot@hotmail.com>
2024-10-16 11:38:57 +0200

    fix: update README for step/

---
+++ crates/step/README.md
@@ -0,0 +1,12 @@
+# nimble-step
+
+nimble-step is a Rust crate that helps you manage participant state changes in deterministic
+simulations. Whether participants are joining, leaving, or reconnecting, this crate provides
+a flexible way to track these changes over time.
+
+Features 🌟
+
+- Track participants joining or leaving the session
+- Handle forced states and reconnections
+- Customize participant actions with your own types
+- Supports serialization/deserialization for efficient serialization
--- crates/step/src/lib.rs
+++ crates/step/src/lib.rs
@@ -40,11 +40,11 @@ impl<T> Step<T> {
     #[must_use]
     pub fn to_octet(&self) -> u8 {
         match self {
-            Step::Forced => 0x01,
-            Step::WaitingForReconnect => 0x02,
-            Step::Joined(_) => 0x03,
-            Step::Left => 0x04,
-            Step::Custom(_) => 0x05,
+            Self::Forced => 0x01,
+            Self::WaitingForReconnect => 0x02,
+            Self::Joined(_) => 0x03,
+            Self::Left => 0x04,
+            Self::Custom(_) => 0x05,
         }
     }
 }
@@ -52,11 +52,11 @@ impl<T> Step<T> {
 impl<T: Display> Display for Step<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            Step::Forced => write!(f, "Forced"),
-            Step::WaitingForReconnect => write!(f, "Forced"),
-            Step::Joined(join_data) => write!(f, "joined {join_data:?}"),
-            Step::Left => write!(f, "Left"),
-            Step::Custom(custom) => write!(f, "Custom({})", custom),
+            Self::Forced => write!(f, "Forced"),
+            Self::WaitingForReconnect => write!(f, "Forced"),
+            Self::Joined(join_data) => write!(f, "joined {join_data:?}"),
+            Self::Left => write!(f, "Left"),
+            Self::Custom(custom) => write!(f, "Custom({})", custom),
         }
     }
 }
@@ -65,8 +65,8 @@ impl<T: Serialize> Serialize for Step<T> {
     fn serialize(&self, stream: &mut impl WriteOctetStream) -> io::Result<()> {
         stream.write_u8(self.to_octet())?;
         match self {
-            Step::Joined(join) => join.serialize(stream),
-            Step::Custom(custom) => custom.serialize(stream),
+            Self::Joined(join) => join.serialize(stream),
+            Self::Custom(custom) => custom.serialize(stream),
             _ => Ok(()),
         }
     }
@@ -76,11 +76,11 @@ impl<T: Deserialize> Deserialize for Step<T> {
     fn deserialize(stream: &mut impl ReadOctetStream) -> io::Result<Self> {
         let step_type = stream.read_u8()?;
         let t = match step_type {
-            0x01 => Step::Forced,
-            0x02 => Step::WaitingForReconnect,
-            0x03 => Step::Joined(JoinedData::deserialize(stream)?),
-            0x04 => Step::Left,
-            0x05 => Step::Custom(T::deserialize(stream)?),
+            0x01 => Self::Forced,
+            0x02 => Self::WaitingForReconnect,
+            0x03 => Self::Joined(JoinedData::deserialize(stream)?),
+            0x04 => Self::Left,
+            0x05 => Self::Custom(T::deserialize(stream)?),
             _ => Err(io::Error::new(
                 io::ErrorKind::InvalidInput,
                 "invalid input, unknown step type",
